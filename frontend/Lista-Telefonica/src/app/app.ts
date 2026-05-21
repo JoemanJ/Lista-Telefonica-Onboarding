7e148d262ff022a4ffecd2fca64c6584107c5f32
JoemanJ <diogodelazare@outlook.com>
2026-05-21 17:01:40 -0300

    feat: adicionada página principal

--- frontend/Lista-Telefonica/src/app/app.ts
+++ frontend/Lista-Telefonica/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { FormContato } from './components/form-contato/form-contato';
+import { ListaContatos } from './components/lista-contatos/lista-contatos';
+import { CardContato } from './components/card-contato/card-contato';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, FormContato, ListaContatos, CardContato],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Lista-Telefonica');
