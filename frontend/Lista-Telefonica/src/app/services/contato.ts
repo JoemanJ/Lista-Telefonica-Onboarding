3882e464958e261c7a97c96e80bd1934b8985925
JoemanJ <diogodelazare@hotmail.com>
2026-05-22 21:30:43 -0300

    feat: adicionados botões para editar e excluir nos cards de contatos

--- frontend/Lista-Telefonica/src/app/services/contato.ts
+++ frontend/Lista-Telefonica/src/app/services/contato.ts
@@ -8,6 +8,7 @@ import { Contato } from '../models/contato.model';
 })
 export class ContatoService {
   private readonly httpClient = inject(HttpClient);
+  // TODO: mudar para o endereço certo da API
   private readonly API_URL = '127.0.0.1:12345/contato';
 
   getTodosContatos(): Observable<Contato[]> {
@@ -46,5 +47,15 @@ export class ContatoService {
   postContato(contato: Contato) {
     //TODO: Mudar isso pra postar o contato na API
     console.log(`Contato salvo: ${contato.nome}, ${contato.telefone}`);
+  }
+
+  putContato(contato: Contato) {
+    //TODO: Mudar isso pra editar o contato na API
+    console.log(`Contato editado: ${contato.nome}, ${contato.telefone}`);
+  }
+
+  deleteContato(contatoId: number){
+    //TODO: Mudar isso pra deletar o contato na API
+    console.log(`Contato com id ${contatoId} deletado`);
   }
 }
